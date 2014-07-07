c0b6a429671e41555ebc3645ee13dfe99f70ee46
Colton J. Provias <cj@coltonprovias.com>
2014-07-07 00:45:18 -0400

    Added column exclusion and overrides

--- sqlalchemy_jsonapi.py
+++ sqlalchemy_jsonapi.py
@@ -9,7 +9,14 @@ from sqlalchemy.orm.base import MANYTOONE
 
 
 class JSONAPIMixin:
+    """
+    Add this mixin to the models that you want to be accessible via your API.
+    """
     _jsonapi_converters = {}
+    _jsonapi_exclude_columns = []
+    _jsonapi_extra_columns = []
+    _jsonapi_column_data_overrides = {}
+    
 
     def _inflector(self, to_inflect):
         """
@@ -26,17 +33,34 @@ class JSONAPIMixin:
         obj = {}
         linked = {}
 
-        # Serialize the columns
-        for column in list(self.__table__.columns):
-            value = getattr(self, column.name)
-            if value is not None and not isinstance(value, (str, int, float, bool)):
-                value = self._jsonapi_converters[type(value).__name__](value)
-            obj[self._inflector(column.name)] = value
+        columns = list(self.__table__.columns)
+
+        column_data = {}
+        for column in columns:
+            if column.name in self._jsonapi_exclude_columns:
+                continue
+            column_data[column.name] = getattr(self, column.name)
+
+        for column in self._jsonapi_extra_columns:
+            column_data[column] = getattr(self, column)
+
+        column_data.update(self._jsonapi_column_data_overrides)
+
+        for name, value in column_data.items():
+            key = self._inflector(name)
+            if value is None:
+                obj[key] = None
+            elif isinstance(value, (str, int, float, bool)):
+                obj[key] = value
+            elif callable(value):
+                obj[key] = value(self)
+            else:
+                obj[key] = self._jsonapi_converters[type(value).__name__](value)
 
         # Serialize the relationships
         obj['links'] = {}
         for relationship in self.__mapper__.relationships:
-            rel_key = getattr(relationship.mapper.class_, '__jsonapi_key__', relationship.mapper.class_.__tablename__)
+            rel_key = getattr(relationship.mapper.class_, '_jsonapi_key', relationship.mapper.class_.__tablename__)
             if rel_key not in linked.keys():
                     linked[self._inflector(rel_key)] = {}
             if relationship.direction == MANYTOONE:
@@ -67,7 +91,7 @@ class JSONAPIMixin:
         """
         For serializing a collection of resources
         """
-        main_key = cls._inflector(cls, getattr(cls, '__jsonapi_key__', cls.__tablename__))
+        main_key = cls._inflector(cls, getattr(cls, '_jsonapi_key', cls.__tablename__))
         to_return = {main_key: [], 'linked': {}, 'meta': {}}
         linked = {}
         for item in collection:
