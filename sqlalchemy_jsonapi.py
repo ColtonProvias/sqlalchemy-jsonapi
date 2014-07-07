fb819bf405e5bd6872c7a795cf132fda072764a5
Colton J. Provias <cj@coltonprovias.com>
2014-07-07 10:44:47 -0400

    Renaming functions to jsonapi_...

--- sqlalchemy_jsonapi.py
+++ sqlalchemy_jsonapi.py
@@ -12,13 +12,13 @@ class JSONAPIMixin:
     """
     Add this mixin to the models that you want to be accessible via your API.
     """
-    _jsonapi_converters = {}
+    jsonapi_converters = {}
 
-    _jsonapi_exclude_columns = []
-    _jsonapi_extra_columns = []
-    _jsonapi_column_data_overrides = {}
+    jsonapi_exclude_columns = []
+    jsonapi_extra_columns = []
+    jsonapi_column_data_overrides = {}
 
-    def _inflector(self, to_inflect):
+    def jsonapi_inflector(self, to_inflect):
         """
         Override this to change the formatting of the keys in the generated
         dict.
@@ -37,17 +37,17 @@ class JSONAPIMixin:
 
         column_data = {}
         for column in columns:
-            if column.name in self._jsonapi_exclude_columns:
+            if column.name in self.jsonapi_exclude_columns:
                 continue
             column_data[column.name] = getattr(self, column.name)
 
-        for column in self._jsonapi_extra_columns:
+        for column in self.jsonapi_extra_columns:
             column_data[column] = getattr(self, column)
 
-        column_data.update(self._jsonapi_column_data_overrides)
+        column_data.update(self.jsonapi_column_data_overrides)
 
         for name, value in column_data.items():
-            key = self._inflector(name)
+            key = self.jsonapi_inflector(name)
             if value is None:
                 obj[key] = None
             elif isinstance(value, (str, int, float, bool)):
@@ -55,38 +55,38 @@ class JSONAPIMixin:
             elif callable(value):
                 obj[key] = value(self)
             else:
-                obj[key] = self._jsonapi_converters[type(value).__name__](value)
+                obj[key] = self.jsonapi_converters[type(value).__name__](value)
 
         # Serialize the relationships
         obj['links'] = {}
         for relationship in self.__mapper__.relationships:
-            rel_key = getattr(relationship.mapper.class_, '_jsonapi_key', relationship.mapper.class_.__tablename__)
+            rel_key = getattr(relationship.mapper.class_, 'jsonapi_key', relationship.mapper.class_.__tablename__)
             if relationship.direction == MANYTOONE:
                 for column in relationship.local_columns:
-                    if self._inflector(column) in obj.keys():
-                        obj['links'][self._inflector(relationship.key)] = str(obj[self._inflector(column)])
-                        del obj[self._inflector(column)]
+                    if self.jsonapi_inflector(column) in obj.keys():
+                        obj['links'][self.jsonapi_inflector(relationship.key)] = str(obj[self.jsonapi_inflector(column)])
+                        del obj[self.jsonapi_inflector(column)]
                 if depth > 0:
                     related_obj = getattr(self, relationship.key)
                     if isinstance(related_obj, JSONAPIMixin):
                         if rel_key not in linked.keys():
-                            linked[self._inflector(rel_key)] = {}
+                            linked[self.jsonapi_inflector(rel_key)] = {}
                         related_obj, related_linked = related_obj.jsonapi_prepare(depth - 1)
                         linked.update(related_linked)
-                        linked[self._inflector(rel_key)][str(related_obj['id'])] = related_obj
+                        linked[self.jsonapi_inflector(rel_key)][str(related_obj['id'])] = related_obj
             elif depth > 0:
                 related_objs = getattr(self, relationship.key)
                 for item in related_objs.all():
                     if not isinstance(item, JSONAPIMixin):
                         continue
-                    if self._inflector(relationship.key) not in obj['links'].keys():
-                        obj['links'][self._inflector(relationship.key)] = []
+                    if self.jsonapi_inflector(relationship.key) not in obj['links'].keys():
+                        obj['links'][self.jsonapi_inflector(relationship.key)] = []
                     if rel_key not in linked.keys():
-                        linked[self._inflector(rel_key)] = {}
-                    obj['links'][self._inflector(relationship.key)].append(str(item.id))
+                        linked[self.jsonapi_inflector(rel_key)] = {}
+                    obj['links'][self.jsonapi_inflector(relationship.key)].append(str(item.id))
                     new_obj, new_linked = item.jsonapi_prepare(depth - 1)
                     linked.update(new_linked)
-                    linked[self._inflector(rel_key)][str(new_obj['id'])] = new_obj
+                    linked[self.jsonapi_inflector(rel_key)][str(new_obj['id'])] = new_obj
         return obj, linked
 
     @classmethod
@@ -94,7 +94,7 @@ class JSONAPIMixin:
         """
         For serializing a collection of resources
         """
-        main_key = cls._inflector(cls, getattr(cls, '_jsonapi_key', cls.__tablename__))
+        main_key = cls.jsonapi_inflector(cls, getattr(cls, 'jsonapi_key', cls.__tablename__))
         to_return = {main_key: [], 'linked': {}, 'meta': {}}
         linked = {}
         for item in collection:
