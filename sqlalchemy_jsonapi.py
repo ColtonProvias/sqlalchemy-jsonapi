39b5dea3cdfaedbb8214292983eae37010f25173
Colton J. Provias <cj@coltonprovias.com>
2014-07-19 22:49:31 -0400

    Updating documentation and getting ready for 0.1 release

--- sqlalchemy_jsonapi.py
+++ sqlalchemy_jsonapi.py
@@ -11,6 +11,10 @@ from sqlalchemy.orm.base import MANYTOONE, ONETOMANY
 
 def as_relationship(to_many=False, linked_key=None, link_key=None,
                     columns=[]):
+    """
+    Decorates methods to add on properties needed to make them act like
+    relationships.
+    """
     def wrapper(f):
         @wraps(f)
         def wrapped(*args, **kwargs):
@@ -30,8 +34,6 @@ class JSONAPIMixin:
     """
     Add this mixin to the models that you want to be accessible via your API.
     """
-    jsonapi_converters = dict()
-
     jsonapi_exclude_columns = []
     jsonapi_extra_columns = []
     jsonapi_column_data_overrides = {}
@@ -41,43 +43,71 @@ class JSONAPIMixin:
     jsonapi_override_relationships = {}
 
     def id(self):
-        raise NotImplementedError
+        """ JSON API recommends having an id for each resource """
+        raise NotImplemented
 
 
 class SkipType(object):
+    """
+    Just a quick thing to help skip conversions.  It's very useful for
+    passwords.
+    """
     pass
 
 
 class JSONAPI:
+    """
+    The JSONAPI Serializer.  This class can be overridden as necessary.
+    """
+    converters = {}
+
     def __init__(self, model):
+        """
+        Let's get started!  The model must be a class of a SQLAlchemy model.
+        """
         self.model = model
 
     def inflector(self, to_inflect):
+        """
+        Override this to inflect the keys in your API.  This is useful for
+        example with ember-json-api, which prefers camelCase.
+        """
         return to_inflect
 
     def convert(self, item, to_convert):
+        """
+        Converts from a provided type to something a little nicer for JSON
+        serialization
+        """
         if to_convert is None:
             return None
         if isinstance(to_convert, (str, int, float, bool)):
             return to_convert
         if callable(to_convert):
             return to_convert(item)
-        if item.jsonapi_converters[type(to_convert)] is not None:
-            converter = item.jsonapi_converters[type(to_convert).__name__]
+        if self.converters[type(to_convert)] is not None:
+            converter = self.converters[type(to_convert).__name__]
             return converter(to_convert)
         return SkipType
 
     def dump_column_data(self, item):
+        """
+        Dumps the data from the columns/properties for a model instance.
+        """
         obj = dict()
         columns = list(item.__table__.columns)
         column_data = dict()
+
         for column in columns:
             if column.name in item.jsonapi_exclude_columns:
                 continue
             column_data[column.name] = getattr(item, column.name)
+
         for column in item.jsonapi_extra_columns:
             column_data[column] = getattr(item, column)
+
         column_data.update(item.jsonapi_column_data_overrides)
+
         for name, value in column_data.items():
             key = self.inflector(name)
             converted = self.convert(item, value)
@@ -86,6 +116,10 @@ class JSONAPI:
         return obj
 
     def dump_relationship_data(self, item, obj, depth):
+        """
+        Dumps all of the data related to relationships, modifying the dumped
+        object as necessary.
+        """
         relationships = dict(map((lambda x: (x.key, x)),
                                  item.__mapper__.relationships))
 
@@ -148,17 +182,29 @@ class JSONAPI:
         return obj, linked
 
     def dump_object(self, item, depth):
+        """
+        Just something to override if you want to change the way data is
+        pre-processed prior to the dump.
+        """
         obj = self.dump_column_data(item)
         return self.dump_relationship_data(item, obj, depth)
 
-    def serialize(self, collection, depth=1):
+    def serialize(self, to_serialize, depth=1):
+        """
+        Call this to return a dict containing the dumped collection or object.
+        """
         api_key = getattr(self.model, 'jsonapi_key', self.model.__tablename__)
         api_key = self.inflector(api_key)
 
         to_return = {api_key: [], 'linked': {}, 'meta': {}}
         linked = dict()
 
-        for item in collection:
+        is_single = isinstance(to_serialize, JSONAPIMixin)
+
+        if is_single:
+            to_serialize = [to_serialize]
+
+        for item in to_serialize:
             dumped = self.dump_object(item, depth)
             if dumped is None:
                 continue
@@ -173,5 +219,8 @@ class JSONAPI:
 
         for key in linked.keys():
             to_return['linked'][key] = list(linked[key].values())
+
+        if is_single:
+            to_return[api_key] = to_return[api_key][0]
 
         return to_return
