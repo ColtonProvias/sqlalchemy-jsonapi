796ae72e829e95a3137e2e60370ce65533619ec5
Kaitlyn Johnson <kjohnson3@laptop-kjohnson3.west.isilon.com>
2017-03-17 17:04:31 -0700

    update model to support delete tests

--- sqlalchemy_jsonapi/unittests/models.py
+++ sqlalchemy_jsonapi/unittests/models.py
@@ -64,7 +64,7 @@ class Comment(Base):
 
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True)
-    post_id = Column(Integer, ForeignKey('posts.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'))
     author_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     content = Column(Text, nullable=False)
 
