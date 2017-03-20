eeb1910dc1d0bc25c11e7eff188e8bd93c3491a5
Kaitlyn Johnson <kjohnson3@laptop-kjohnson3.west.isilon.com>
2017-03-20 12:08:26 -0700

    add test for checking cascade

--- sqlalchemy_jsonapi/unittests/test_serializer_delete_resource.py
+++ sqlalchemy_jsonapi/unittests/test_serializer_delete_resource.py
@@ -42,3 +42,29 @@ class DeleteResource(testcases.SqlalchemyJsonapiTestCase):
                 self.session, {}, 'non-existant', 1)
 
         self.assertEqual(error.exception.status_code, 404)
+
+    def test_delete_resource_cascade_with_one_many_relationship(self):
+        """Delete a resource with a cascade and one-to-many relationship.
+
+        Ensure all referencing models are removed.
+        """
+        user = models.User(
+            first='Sally', last='Smith',
+            password='password', username='SallySmith1')
+        self.session.add(user)
+        blog_post = models.Post(
+            title='This Is A Title', content='This is the content',
+            author_id=user.id, author=user)
+        self.session.add(blog_post)
+        comment = models.Comment(
+            content='This is comment 1', author_id=user.id,
+            post_id=blog_post.id, author=user, post=blog_post)
+        self.session.add(comment)
+        self.session.commit()
+
+        models.serializer.delete_resource(self.session, {}, 'users', 1)
+
+        post = self.session.query(models.Post).get(1)
+        comment = self.session.query(models.Comment).get(1)
+        self.assertEqual(post, None)
+        self.assertEqual(comment, None)
