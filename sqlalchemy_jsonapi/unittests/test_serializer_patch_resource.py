6893f0977d029ae7030b32f1c731241874d5802f
Kaitlyn Johnson <kjohnson3@laptop-kjohnson3.west.isilon.com>
2017-03-22 10:06:44 -0700

    add patch_resource test

--- sqlalchemy_jsonapi/unittests/test_serializer_patch_resource.py
+++ sqlalchemy_jsonapi/unittests/test_serializer_patch_resource.py
@@ -19,7 +19,6 @@ class PatchResource(testcases.SqlalchemyJsonapiTestCase):
             title='This Is A Title', content='This is the content')
         self.session.add(blog_post)
         self.session.commit()
-
         payload = {
             'data': {
                 'type': 'posts',
@@ -44,6 +43,7 @@ class PatchResource(testcases.SqlalchemyJsonapiTestCase):
         self.assertEqual(blog_post.author.id, user.id)
         self.assertEqual(blog_post.author, user)
 
+    @testcases.fragile
     def test_patch_resource_response(self):
         """Patch resource response returns resource and 200."""
         user = models.User(
@@ -148,8 +148,8 @@ class PatchResource(testcases.SqlalchemyJsonapiTestCase):
         self.assertEqual(error.exception.detail, expected_detail)
         self.assertEqual(error.exception.status_code, 400)
 
-    def test_patch_resource_with_missing_type(self):
-        """Patch resource with missing type results in 400.
+    def test_patch_resource_with_mismatched_id(self):
+        """Patch resource with mismatched id results in 400.
 
         A BadRequestError is raised.
         """
@@ -164,6 +164,7 @@ class PatchResource(testcases.SqlalchemyJsonapiTestCase):
         payload = {
             'data': {
                 'type': 'posts',
+                'id': 2,
                 'attributes': {
                     'title': 'This is a new title'
                 },
@@ -182,6 +183,86 @@ class PatchResource(testcases.SqlalchemyJsonapiTestCase):
             models.serializer.patch_resource(
                 self.session, payload, 'posts', blog_post.id)
 
-        expected_detail = 'Missing type or id'
+        expected_detail = 'IDs do not match'
+        self.assertEqual(error.exception.detail, expected_detail)
+        self.assertEqual(error.exception.status_code, 400)
+
+    def test_patch_resource_with_mismatched_type(self):
+        """Patch resource with mismatched type results in 400.
+
+        A BadRequestError is raised.
+        """
+        user = models.User(
+            first='Sally', last='Smith',
+            password='password', username='SallySmith1')
+        self.session.add(user)
+        blog_post = models.Post(
+            title='This Is A Title', content='This is the content')
+        self.session.add(blog_post)
+        self.session.commit()
+        payload = {
+            'data': {
+                'type': 'comments',
+                'id': blog_post.id,
+                'attributes': {
+                    'title': 'This is a new title'
+                },
+                'relationships': {
+                    'author': {
+                        'data': {
+                            'type': 'users',
+                            'id': user.id
+                        }
+                    }
+                }
+            }
+        }
+
+        with self.assertRaises(errors.BadRequestError) as error:
+            models.serializer.patch_resource(
+                self.session, payload, 'posts', blog_post.id)
+
+        expected_detail = 'Type does not match'
+        self.assertEqual(error.exception.detail, expected_detail)
+        self.assertEqual(error.exception.status_code, 400)
+
+    def test_patch_resource_relationship_field_not_found(self):
+        """Patch resource with unknown relationship field returns 400.
+
+        A BadRequestError is raised.
+        """
+        user = models.User(
+            first='Sally', last='Smith',
+            password='password', username='SallySmith1')
+        self.session.add(user)
+        blog_post = models.Post(
+            title='This Is A Title', content='This is the content',
+            author_id=user.id, author=user)
+        self.session.add(blog_post)
+        comment = models.Comment(
+            content='This is a comment', author_id=user.id,
+            post_id=blog_post.id, author=user, post=blog_post)
+        self.session.add(comment)
+        self.session.commit()
+        payload = {
+            'data': {
+                'type': 'posts',
+                'id': blog_post.id,
+                'relationships': {
+                    'nonexistant': {
+                        'data': {
+                            'type': 'users',
+                            'id': user.id
+                        }
+                    }
+                }
+            }
+        }
+
+        with self.assertRaises(errors.BadRequestError) as error:
+            models.serializer.patch_resource(
+                self.session, payload, 'posts', blog_post.id)
+
+        expected_detail = 'nonexistant not relationships for posts.1'
         self.assertEqual(error.exception.detail, expected_detail)
         self.assertEqual(error.exception.status_code, 400)
