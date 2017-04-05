045b39c260e008002ddc92ee87e79939cae7c7da
Kaitlyn Johnson <kjohnson3@laptop-kjohnson3.west.isilon.com>
2017-04-05 11:46:34 -0700

    Add functionality of having model names dasherized upon serialization of model.

--- sqlalchemy_jsonapi/declarative/serializer.py
+++ sqlalchemy_jsonapi/declarative/serializer.py
@@ -1,44 +1,53 @@
 """A serializer for serializing SQLAlchemy models to JSON API spec."""
 
+import datetime
+
+from inflection import dasherize, underscore
+
 
 class JSONAPISerializer(object):
     """A JSON API serializer that serializes SQLAlchemy models."""
 
     def serialize(self, resources):
-        """Serialize resource(s) according to JSON API spec.
+        """Serialize resource(s) according to json-api spec."""
+        try:
+            self.fields
+            self.model
+            self.dasherize
+        except AttributeError:
+            raise
 
-        Args:
-            resources: SQLAlchemy model or SQLAlchemy query
-        """
         if 'id' not in self.fields:
-            raise AttributeError
-
-        serialized_object = {
-            'included': [],
-            'jsonapi': {
-                'version': '1.0'
-            },
+            raise ValueError
+        serialized = {
             'meta': {
                 'sqlalchemy_jsonapi_version': '4.0.9'
+            },
+            'jsonapi': {
+                'version': '1.0'
             }
         }
-        if isinstance(resources, self.model):
-            serialized_object['data'] = self._render_resource(resources)
-        else:
-            serialized_object['data'] = []
+        # Determine multiple resources by checking for SQLAlchemy query count.
+        if hasattr(resources, 'count'):
+            serialized['data'] = []
             for resource in resources:
-                serialized_object['data'].append(
+                serialized['data'].append(
                     self._render_resource(resource))
+        else:
+            serialized['data'] = self._render_resource(resources)
 
-        return serialized_object
+        return serialized
 
     def _render_resource(self, resource):
-        """Renders a resource's top level members based on JSON API spec.
+        """Renders a resource's top level members based on json-api spec.
 
         Top level members include:
             'id', 'type', 'attributes', 'relationships'
         """
-        # Must not render a resource that has same named attributes as different model.
+        if not resource:
+            return None
+        # Must not render a resource that has same named
+        # attributes as different model.
         if not isinstance(resource, self.model):
             raise TypeError
 
@@ -54,12 +63,31 @@ class JSONAPISerializer(object):
     def _render_attributes(self, resource):
         """Render the resources's attributes."""
         attributes = {}
-        related_models = resource.__mapper__.relationships.keys()
+        attrs_to_ignore = set()
+
+        for key, relationship in resource.__mapper__.relationships.items():
+            attrs_to_ignore.update(set(
+                [column.name for column in relationship.local_columns]).union(
+                    {key}))
+
+        if self.dasherize:
+            mapped_fields = {x: dasherize(underscore(x)) for x in self.fields}
+        else:
+            mapped_fields = {x: x for x in self.fields}
+
         for attribute in self.fields:
-            if attribute in related_models or attribute == 'id':
+            if attribute == 'id':
                 continue
+            # Per json-api spec, we cannot render foreign keys
+            # or relationsips in attributes.
+            if attribute in attrs_to_ignore:
+                raise AttributeError
             try:
-                attributes[attribute] = getattr(resource, attribute)
+                value = getattr(resource, attribute)
+                if isinstance(value, datetime.datetime):
+                    attributes[mapped_fields[attribute]] = value.isoformat()
+                else:
+                    attributes[mapped_fields[attribute]] = value
             except AttributeError:
                 raise
 
@@ -69,13 +97,21 @@ class JSONAPISerializer(object):
         """Render the resource's relationships."""
         relationships = {}
         related_models = resource.__mapper__.relationships.keys()
+        if self.dasherize:
+            mapped_relationships = {
+                x: dasherize(underscore(x)) for x in related_models}
+        else:
+            mapped_relationships = {x: x for x in related_models}
+
         for model in related_models:
-            relationships[model] = {
+            relationships[mapped_relationships[model]] = {
                 'links': {
                     'self': '/{}/{}/relationships/{}'.format(
-                        resource.__tablename__, resource.id, model),
+                        resource.__tablename__, resource.id,
+                        mapped_relationships[model]),
                     'related': '/{}/{}/{}'.format(
-                        resource.__tablename__, resource.id, model)
+                        resource.__tablename__, resource.id,
+                        mapped_relationships[model])
                 }
             }
 
